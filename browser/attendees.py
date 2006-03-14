f464ef362082210f2ca0b53bf1429031cb0b5feb
Rocky Burt <rocky@serverzen.com>
2006-03-14 14:41:57 +0000

    Hearing view now contains listings of Members, Council Members, and Speakers.
    svn path=/minaraad/trunk/; revision=28520

--- browser/attendees.py
+++ browser/attendees.py
@@ -6,13 +6,10 @@ from Products.minaraad.interfaces import IAttendeeManager
 
 class IAttendeesManagerView(Interface):
 
-    def register():
-        pass
-    
-    def unregister():
-        pass
-    
     def isRegistered():
+        pass
+
+    def groupedAttendees(self):
         pass
 
 
@@ -54,3 +51,29 @@ class AttendeesManagerView(AbstractView):
         isAnonymous = memberTool.isAnonymousUser()
         member = memberTool.getAuthenticatedMember()
         return (not isAnonymous) and member.getMemberId() in self.manager.attendees()
+    
+    def groupedAttendees(self):
+        attendees = {'council_members': [],
+                     'members': []}
+                     
+        memTool = getToolByName(self.context, 'portal_membership')
+        
+        for memberId in self.manager.attendees():
+            member = memTool.getMemberById(memberId)
+            nice = member.getProperty('firstname', '') + ' ' + \
+                   member.getProperty('fullname', '')
+            nice = nice.strip()
+            if not nice:
+                nice = memberId
+            
+            roles = member.getRolesInContext(self.context)
+            if 'Council Member' in roles:
+                group = attendees['council_members']
+            else:
+                group = attendees['members']
+            
+            group.append({'memberId': memberId,
+                          'niceName': nice,
+                          'member': member})
+        
+        return attendees
