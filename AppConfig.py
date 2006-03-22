03f944b70b87e18d5e8df55de69c45293f7f2985
Mirella van Teulingen <spacebabe77@gmail.com>
2006-03-22 11:51:24 +0000

    - added layout for study listing view - added study listing view to Appconfig - fixed som minor template bugs
    svn path=/minaraad/trunk/; revision=28588

--- AppConfig.py
+++ AppConfig.py
@@ -74,7 +74,8 @@ TYPES_TO_LIST = ['Folder']
 # be added in the view dropdown.
 EXTRA_VIEWS = {
     'Folder': ['hearing_listing_view', 'advisory_listing_view', 
-               'newsletter_listing_view', 'pressrelease_listing_view'],
+               'newsletter_listing_view', 'pressrelease_listing_view',
+               'study_listing_view'],
     }
 
 # SELECT_VIEWS is a dictionary, the keys are folder names. The value
@@ -97,6 +98,7 @@ SELECT_VIEWS = {
     '/persberichten/pressr_2004' : 'pressrelease_listing_view',
     '/persberichten/pressr_2003' : 'pressrelease_listing_view',
     '/persberichten/pressr_2002' : 'pressrelease_listing_view',
+    '/studies': 'study_listing_view',
     }
 
 #FCKeditor set height
