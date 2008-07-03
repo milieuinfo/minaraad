3a46a16f2edf0bb9e888833958d9289c0d78a1f7
Maurits van Rees <maurits@vanrees.org>
2008-07-03 13:17:39 +0000

    Made form for the apparently already existing @@minaraad_service view so members without an email address can be deleted.
    svn path=/minaraad/trunk/; revision=29054

--- browser/service.py
+++ browser/service.py
@@ -11,20 +11,34 @@ from Products.minaraad.config import TITLE_VOCAB
 import logging
 logger = logging.getLogger('Minaraad Service Utilities')
 
+
 class ServiceUtils(BrowserView):
+
+    members_deleted = None
+
+    def __call__(self):
+        if self.request.get('REQUEST_METHOD', 'GET').upper() == 'POST' \
+                and self.request.get('form.button.DeleteMembers'):
+            self.members_deleted = self.deleteMembersWithEmptyEmail()
+        return self.index()
 
     def deleteMembersWithEmptyEmail(self):
         'Delete all members with no email address'
         mt = getToolByName(self, 'portal_membership')
         acl = getToolByName(self, 'acl_users')
         all_members = mt.listMembers()
-        members_without_email = [m for m in all_members if not m.getProperty('email')]
-        logger.info('Found %i members without email, proceed to delete...' % (len(members_without_email), ))
+        members_without_email = [m for m in all_members
+                                 if not m.getProperty('email')
+                                 or not '@' in m.getProperty('email')]
+
+        logger.info('Found %i members without email, proceed to delete...' %
+                    (len(members_without_email), ))
         for member in members_without_email:
             id = member.getId()
             logger.info('Deleting member "%s"' % (id, ))
             acl.userFolderDelUsers([id])
-        logger.info('Finished deleting %i members' % (len(members_without_email), ))
+        logger.info('Finished deleting %i members' %
+                    (len(members_without_email), ))
         return 'Deleted %i members' % (len(members_without_email), )
 
     def resetMemberDataTitle(self):
@@ -39,6 +53,8 @@ class ServiceUtils(BrowserView):
         'add last_modification_date to the memberdata'
         md = getToolByName(self, 'portal_memberdata')
         if 'last_modification_date' not in md.propertyIds():
-            md.manage_addProperty('last_modification_date', '2000/01/01', 'date')
-        logger.info('"last_modification_date" property of memberdata tool is added.')
-        return '"last_modification_date" property of memberdata tool is added.'
+            md.manage_addProperty('last_modification_date', '2000/01/01',
+                                  'date')
+        msg = '"last_modification_date" property of memberdata tool is added.'
+        logger.info(msg)
+        return msg
