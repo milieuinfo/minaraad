20f258adee117d98d22ae89c5207d8925dd417c0
Reinout van Rees <reinout@vanrees.org>
2008-12-03 15:40:19 +0000

    Changed goal field of Hearing to a richtext widget with accompagnying   template changes
    svn path=/minaraad/trunk/; revision=29158

--- content/Hearing.py
+++ content/Hearing.py
@@ -36,14 +36,21 @@ schema = Schema((
 ),
 )
 
-# TODO: add body-like text.
-
 Hearing_schema = BaseFolderSchema.copy() + \
     getattr(MREvent, 'schema', Schema(())).copy() + \
     getattr(EmailMixin, 'schema', Schema(())).copy() + \
     schema.copy()
 Hearing_schema.moveField('theme', after="goal")
 Hearing_schema.moveField('mot', after="theme")
+# 'goal' (from MREvent) should use a rich text editor on this contenttype.
+Hearing_schema['goal'].widget = RichWidget(
+    label='Goal',
+    label_msgid='minaraad_label_goal',
+    i18n_domain='minaraad',
+    )
+Hearing_schema['goal'].default_output_type = 'text/html'
+Hearing_schema['goal'].allowable_content_types = (
+    'text/plain', 'text/structured', 'text/html', 'application/msword',)
 
 
 class IHearing(Interface):
