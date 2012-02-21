4ed6ff868a77aa678f80760e47fef50ec1417df7
Maurits van Rees <maurits@vanrees.org>
2012-02-21 16:19:10 +0000

    Added fabfile.py for testing.
    svn path=/minaraad/trunk/; revision=31139

---
+++ fabfile.py
@@ -0,0 +1,34 @@
+from fabric.api import env, run  # local, put, cd
+#from fabric.decorators import hosts
+
+
+def ontwikkel():
+    env.hosts = [
+        'zope@plone-minaraad-on-1-mgt.mmis.be',
+        'zope@plone-minaraad-on-2-mgt.mmis.be',
+        ]
+
+
+def oefen():
+    env.hosts = [
+        'zope@plone-minaraad-oe-1-mgt.mmis.be',
+        'zope@plone-minaraad-oe-2-mgt.mmis.be',
+        ]
+
+
+def productie():
+    env.hosts = [
+        'zope@plone-minaraad-pr-1-mgt.mmis.be',
+        'zope@plone-minaraad-pr-2-mgt.mmis.be',
+        ]
+
+
+def update_ontwikkel():
+    """Update Plone on ontwikkel.
+
+    You should run this with 'fab ontwikkel update_ontwikkel'.
+    """
+    run('cd ~/buildout')
+    run('bin/supervisorctl shutdown')
+    run('svn up')
+    run('bin/supervisord')
