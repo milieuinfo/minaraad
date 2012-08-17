68f1eddc70f9663b76df8e45242ff34693b63ac7
Maurits van Rees <maurits@vanrees.org>
2012-08-17 15:30:14 +0000

    Read HTTP_PROXY from the environment when running the buildout.
    Then add it to the generated zope.conf.  This avoids the problem the
    this environment variable does not exist when zope is started at
    reboot time by cron, which would cause errors checking the captcha on
    the registration form.
    
    svn path=/minaraad/trunk/; revision=31204

--- src/Products.minaraad/Products/minaraad/patches.py
+++ src/Products.minaraad/Products/minaraad/patches.py
@@ -1,11 +1,14 @@
-import os, urllib, urllib2
+import os
+import urllib2
+
 from recaptcha.client import captcha
+
 
 def submit(*args, **kwargs):
     """
     Patch for recaptcha.client.captcha.submit to handle http proxy.
     """
-    if 'http_proxy' in os.environ:
+    if os.environ.get('HTTP_PROXY'):
         proxy_support = urllib2.ProxyHandler()
         opener = urllib2.build_opener(proxy_support)
         urllib2.install_opener(opener)
@@ -16,12 +19,14 @@ def patch_captcha():
     captcha.old_submit = captcha.submit
     captcha.submit = submit
 
+
 def unpatch_captcha():
     captcha.submit = captcha.old_submit
 
 
 def patch_all():
     patch_captcha()
+
 
 def unpatch_all():
     unpatch_captcha()
