f98a0bcc403338e67f12f4f01aaaa962522f7123
Maurits van Rees <maurits@vanrees.org>
2013-02-01 16:53:03 +0000

    i18ndude: do plone files too.
    svn path=/minaraad/trunk/; revision=31570

--- src/Products.minaraad/Products/minaraad/rebuild-i18n.sh
+++ src/Products.minaraad/Products/minaraad/rebuild-i18n.sh
@@ -6,6 +6,11 @@ for po in locales/*/LC_MESSAGES/minaraad.po; do
     i18ndude sync --pot locales/minaraad.pot $po
 done
 
+i18ndude rebuild-pot --pot locales/plone.pot --merge locales/plone-manual.pot --create plone profiles
+for po in locales/*/LC_MESSAGES/plone.po; do
+    i18ndude sync --pot locales/plone.pot $po
+done
+
 #echo "Reporting some statistics..."
 # Find places that are missing an "i18n:translate" or
 # "i18n:attributes" tag.
