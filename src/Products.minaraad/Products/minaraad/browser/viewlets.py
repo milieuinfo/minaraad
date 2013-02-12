5cfdb68cc0bfc8cff4ce579729b733c219882780
Vincent Pretre <v.pretre@zestsoftware.nl>
2013-02-12 11:24:59 +0000

    Use sitemap for navigation viewlet
    svn path=/minaraad/trunk/; revision=31588

--- src/Products.minaraad/Products/minaraad/browser/viewlets.py
+++ src/Products.minaraad/Products/minaraad/browser/viewlets.py
@@ -9,37 +9,11 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 class MinaGlobalSectionsViewlet(GlobalSectionsViewlet):
     index = ViewPageTemplateFile('sections.pt')
 
-    def get_portal_tabs(self):
+    def get_hidden_titles(self):
         portal = getSite()
-        tabs = self.portal_tabs
-
         portal_props = getToolByName(portal, 'portal_properties')
-        hidden_titles = portal_props.navtree_properties.titlesNotInTabs
+        return portal_props.navtree_properties.titlesNotInTabs
 
-        for tab in tabs:
-            tab_id = tab.get('id', None)
-            if tab.get('title', '') in hidden_titles:
-                continue
-
-            folder = portal.get(tab_id, None)
-            if folder is None:
-                continue
-
-            if folder.portal_type not in ['Folder', 'DigiBib']:
-                continue
-
-            tab['children'] = []
-
-            for el in folder.contentValues():
-                if not IFolder.providedBy(el):
-                    continue
-
-                if getattr(el, 'exclude_from_nav', False):
-                    continue
-
-                try:
-                    tab['children'].append(el)
-                except:
-                    pass
-
-        return tabs
+    def get_sitemap(self):
+        portal = getSite()
+        return self.context.plone_utils.createSitemap(portal)
