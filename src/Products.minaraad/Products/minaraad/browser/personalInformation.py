c97435c7a83b58a3a697474e0530b9ae350a023c
Vincent Pretre <v.pretre@zestsoftware.nl>
2013-02-19 16:09:04 +0000

    Filter out default plone fields in personal-informations view
    svn path=/minaraad/trunk/; revision=31643

---
+++ src/Products.minaraad/Products/minaraad/browser/personalInformation.py
@@ -0,0 +1,9 @@
+from plone.app.users.browser.personalpreferences import UserDataPanel
+
+class CustomizedUserDataPanel(UserDataPanel):
+    def __init__(self, context, request):
+        super(CustomizedUserDataPanel, self).__init__(context, request)
+
+        omited_fields = ['home_page', 'description', 'location', 'portrait', 'pdelete']
+        for f_name in omited_fields:
+            self.form_fields = self.form_fields.omit(f_name)
