548a69ff0e3f543cd472546f4a0fa324e461f34f
Maurits van Rees <maurits@vanrees.org>
2013-06-26 09:41:33 +0000

    Update the last modification date when changing a member.
    Done with a patch on notifyModified, instead of the previous prefs_user_edit.cpy
    that was no longer used in Plone 4.
    We want an event when a user is modified, really, but alas, nothing is fired.
    
    svn path=/minaraad/trunk/; revision=31903

--- src/Products.minaraad/Products/minaraad/patches.py
+++ src/Products.minaraad/Products/minaraad/patches.py
@@ -2,6 +2,9 @@ import os
 import urllib2
 
 from recaptcha.client import captcha
+from DateTime import DateTime
+from Products.CMFCore.MemberDataTool import MemberData
+from Products.PlonePAS.interfaces.propertysheets import IMutablePropertySheet
 
 
 def submit(*args, **kwargs):
@@ -15,6 +18,38 @@ def submit(*args, **kwargs):
     return captcha.old_submit(*args, **kwargs)
 
 
+def set_last_modification_date(member):
+    """Set the last_modification_date of the member.
+
+    Adapted from PlonePAS.tools.memberdata.MemberData.setMemberProperties.
+    """
+    user = member.getUser()
+    sheets = getattr(user, 'getOrderedPropertySheets', lambda: None)()
+
+    # We won't always have PlonePAS users, due to acquisition,
+    # nor are guaranteed property sheets
+    if not sheets:
+        return
+
+    # If we got this far, we have a PAS and some property sheets.
+    property_id = 'last_modification_date'
+    value = DateTime()
+    for sheet in sheets:
+        if not sheet.hasProperty(property_id):
+            continue
+        if IMutablePropertySheet.providedBy(sheet):
+            print 'setting last modification date'
+            sheet.setProperty(user, property_id, value)
+            break
+
+
+def notifyModified(self):
+    print 'last modified 1:', self.getProperty('last_modification_date')
+    set_last_modification_date(self)
+    print 'last modified: 2', self.getProperty('last_modification_date')
+    return self._orig_notifyModified()
+
+
 def patch_captcha():
     captcha.old_submit = captcha.submit
     captcha.submit = submit
@@ -24,9 +59,20 @@ def unpatch_captcha():
     captcha.submit = captcha.old_submit
 
 
+def patch_memberdata():
+    MemberData._orig_notifyModified = MemberData.notifyModified
+    MemberData.notifyModified = notifyModified
+
+
+def unpatch_memberdata():
+    MemberData.notifyModified = MemberData._orig_notifyModified
+
+
 def patch_all():
     patch_captcha()
+    patch_memberdata()
 
 
 def unpatch_all():
     unpatch_captcha()
+    unpatch_memberdata()
