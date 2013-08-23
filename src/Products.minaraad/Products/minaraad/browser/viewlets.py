798190d1bf84a3ebca927323f5b019fce2603e4d
Maurits van Rees <maurits@vanrees.org>
2013-08-23 09:29:09 +0000

    Fix possible AttributeError during inline validation.
    This is while trying to show an error, which tries to display a complete page,
    but the previous getSite call would not return the actual portal,
    so it got an error trying to show a sitemap for our sections viewlet.
    Kinda weird.
    
    svn path=/minaraad/trunk/; revision=31937

--- src/Products.minaraad/Products/minaraad/browser/viewlets.py
+++ src/Products.minaraad/Products/minaraad/browser/viewlets.py
@@ -1,21 +1,30 @@
-from zope.component.hooks import getSite
-
+from Acquisition import aq_inner
 from Products.CMFCore.utils import getToolByName
-from plone.app.layout.viewlets.common import GlobalSectionsViewlet
-
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets.common import GlobalSectionsViewlet
+from zope.component import getMultiAdapter
 
 
 class MinaGlobalSectionsViewlet(GlobalSectionsViewlet):
     index = ViewPageTemplateFile('sections.pt')
 
+    def _get_portal(self):
+        # Just a little helper function.  We used getSite() from
+        # zope.component.hooks first, but that returned a
+        # ValidationView when doing inline validation.
+        context = aq_inner(self.context)
+        pps = getMultiAdapter((context, self.request),
+                              name='plone_portal_state')
+        return pps.portal()
+
     def get_hidden_titles(self):
-        portal = getSite()
+        portal = self._get_portal()
         portal_props = getToolByName(portal, 'portal_properties')
         if not portal_props.navtree_properties.hasProperty('titlesNotInTabs'):
             return []
         return portal_props.navtree_properties.titlesNotInTabs
 
     def get_sitemap(self):
-        portal = getSite()
-        return self.context.plone_utils.createSitemap(portal)
+        portal = self._get_portal()
+        plone_utils = getToolByName(portal, 'plone_utils')
+        return plone_utils.createSitemap(portal)
