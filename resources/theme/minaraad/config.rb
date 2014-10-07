2a715a1a6baecd2f06c93d1c9d9b7083c8c2d1f1
Maurits van Rees <maurits@vanrees.org>
2014-10-07 08:18:00 +0000

    sass: compact
    svn path=/minaraad/trunk/; revision=32076

--- resources/theme/minaraad/config.rb
+++ resources/theme/minaraad/config.rb
@@ -9,7 +9,7 @@ images_dir = "img"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-#output_style = :compact
+output_style = :compact
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
