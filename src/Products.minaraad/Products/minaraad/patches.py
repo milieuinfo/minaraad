7088e29eac59c18938779867f2fd69461ca33c26
Maurits van Rees <maurits@vanrees.org>
2015-04-20 20:48:46 +0000

    First try at improving deletion of users.
    This takes too long and especially takes too much memory.
    Patch the MembershipTool.
    Not really progress yet...
    
    svn path=/minaraad/trunk/; revision=32148

--- src/Products.minaraad/Products/minaraad/patches.py
+++ src/Products.minaraad/Products/minaraad/patches.py
@@ -1,10 +1,16 @@
 import os
+import transaction
 import urllib2
 
-from recaptcha.client import captcha
+from Acquisition import aq_base
 from DateTime import DateTime
 from Products.CMFCore.MemberDataTool import MemberData
+from Products.CMFCore.MembershipTool import MembershipTool
+from Products.CMFCore.permissions import ChangeLocalRoles
+from Products.CMFCore.utils import _checkPermission
+from Products.CMFCore.utils import getToolByName
 from Products.PlonePAS.interfaces.propertysheets import IMutablePropertySheet
+from recaptcha.client import captcha
 
 
 def submit(*args, **kwargs):
@@ -42,6 +48,69 @@ def set_last_modification_date(member):
             break
 
 
+# security.declareProtected(View, 'deleteLocalRoles')
+# @postonly
+def deleteLocalRoles(self, obj, member_ids, reindex=1, recursive=0,
+                     REQUEST=None, obj_count=0):
+    """ Delete local roles of specified members.
+
+    This takes far too much memory.  See if we can reduce this.
+
+    Example with ZopeFindAndApply:
+
+    def fixOwnerTuple(obj, path):
+        old = obj.getOwnerTuple()
+        if old and old[0][-1] == 'portal_memberdata':
+            new = (['acl_users'], old[1])
+            logger.info('Repairing %s: %r -> %r' % (path, old, new))
+            obj._owner = new
+    portal.ZopeFindAndApply(portal, search_sub=True, apply_func=fixOwnerTuple)
+
+    Well, might want to try the catalog.
+
+    """
+    if reindex:
+        print("Using minaraad patch of deleteLocalRoles.")
+    from time import time
+    time1 = time()
+    deleteLocalRolesSingleObject(obj, member_ids)
+    time2 = time()
+    obj_count += 1
+    if reindex:
+        print("main del local roles: %f" % (time2 - time1))
+    if obj_count % 100 == 0:
+        print("Saving sub transaction or savepoint. Object count %d" % obj_count)
+        transaction.savepoint(optimistic=True)
+        # Note that when searching on the Internet, you see references
+        # to committing a sub transaction like this, but that is only
+        # for ZODB 4 it seems.  It certainly fails with a TypeError
+        # for us: transaction.commit(True)
+    if recursive and hasattr(aq_base(obj), 'contentValues'):
+        for subobj in obj.contentValues():
+            obj_count = self.deleteLocalRoles(subobj, member_ids, 0, 1, obj_count=obj_count)
+
+    time3 = time()
+    if reindex:
+        print("delete local roles everywhere: %f" % (time3 - time2))
+    if reindex and hasattr(aq_base(obj), 'reindexObjectSecurity'):
+        # reindexObjectSecurity is always recursive
+        obj.reindexObjectSecurity()
+    time4 = time()
+    if reindex:
+        print("reindex object security: %f" % (time4 - time3))
+    return obj_count
+
+
+def deleteLocalRolesSingleObject(obj, member_ids):
+    """ Delete local roles of specified members.
+    """
+    if _checkPermission(ChangeLocalRoles, obj):
+        for member_id in member_ids:
+            if obj.get_local_roles_for_userid(userid=member_id):
+                obj.manage_delLocalRoles(userids=member_ids)
+                break
+
+
 def notifyModified(self):
     set_last_modification_date(self)
     return self._orig_notifyModified()
@@ -65,11 +134,22 @@ def unpatch_memberdata():
     MemberData.notifyModified = MemberData._orig_notifyModified
 
 
+def patch_membershiptool():
+    MembershipTool._orig_deleteLocalRoles = MembershipTool.deleteLocalRoles
+    MembershipTool.deleteLocalRoles = deleteLocalRoles
+
+
+def unpatch_membershiptool():
+    MembershipTool.deleteLocalRoles = MembershipTool._orig_deleteLocalRoles
+
+
 def patch_all():
     patch_captcha()
     patch_memberdata()
+    patch_membershiptool()
 
 
 def unpatch_all():
     unpatch_captcha()
     unpatch_memberdata()
+    unpatch_membershiptool()
