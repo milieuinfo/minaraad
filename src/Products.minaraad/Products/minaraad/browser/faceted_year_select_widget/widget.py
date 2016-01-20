f5d671400e10731ecda91c9b573046300463a064
Coen van der Kamp <c.van.der.kamp@zestsoftware.nl>
2016-01-20 15:52:43 +0100

    Add year select widget

--- src/Products.minaraad/Products/minaraad/browser/faceted_year_select_widget/widget.py
+++ src/Products.minaraad/Products/minaraad/browser/faceted_year_select_widget/widget.py
@@ -1,64 +1,17 @@
 """ Select widget
 """
-from Products.Archetypes.public import Schema
-from Products.Archetypes.public import BooleanField
-from Products.Archetypes.public import StringField
-from Products.Archetypes.public import StringWidget
-from Products.Archetypes.public import SelectionWidget
-from Products.Archetypes.public import BooleanWidget
-
-from eea.facetednavigation.dexterity_support import normalize as atdx_normalize
+from BTrees.IIBTree import weightedIntersection, IISet
+from DateTime import DateTime
+from eea.facetednavigation import EEAMessageFactory as _
+from eea.facetednavigation.interfaces import IFacetedCatalog
 from eea.facetednavigation.widgets import ViewPageTemplateFile
 from eea.facetednavigation.widgets.widget import CountableWidget
-from eea.facetednavigation import EEAMessageFactory as _
+from Products.Archetypes.public import Schema
+from Products.CMFCore.utils import getToolByName
+from zope.component import queryUtility
 
 
-EditSchema = Schema((
-    StringField('index',
-        schemata="default",
-        required=True,
-        vocabulary_factory='eea.faceted.vocabularies.CatalogIndexes',
-        widget=SelectionWidget(
-            label=_(u'Catalog index'),
-            description=_(u'Catalog index to use for search'),
-            i18n_domain="eea"
-        )
-    ),
-    StringField('vocabulary',
-        schemata="default",
-        vocabulary_factory='eea.faceted.vocabularies.PortalVocabularies',
-        widget=SelectionWidget(
-            label=_(u"Vocabulary"),
-            description=_(u'Vocabulary to use to render widget items'),
-        )
-    ),
-    StringField('catalog',
-        schemata="default",
-        vocabulary_factory='eea.faceted.vocabularies.UseCatalog',
-        widget=SelectionWidget(
-            format='select',
-            label=_(u'Catalog'),
-            description=_(u"Get unique values from catalog "
-                        u"as an alternative for vocabulary"),
-        )
-    ),
-    BooleanField('sortreversed',
-        schemata="display",
-        widget=BooleanWidget(
-            label=_(u"Reverse options"),
-            description=_(u"Sort options reversed"),
-        )
-    ),
-    StringField('default',
-        schemata="default",
-        widget=StringWidget(
-            size=25,
-            label=_(u'Default value'),
-            description=_(u'Default selected item'),
-            i18n_domain="eea"
-        )
-    ),
-))
+EditSchema = Schema(())
 
 
 class Widget(CountableWidget):
@@ -74,6 +27,81 @@ class Widget(CountableWidget):
     index = ViewPageTemplateFile('widget.pt')
     edit_schema = CountableWidget.edit_schema.copy() + EditSchema
 
+    def default(self):
+        return ""
+
+    @property
+    def years(self):
+        catalog = getToolByName(self.context, 'portal_catalog')
+
+        # Sometimes objects are created with a effective date of
+        # '1000-01-01 00:00:00'. Resulting in 1000+ values.
+        # Therefore we limit `fist` to a reasonable effective date.
+        first = catalog.searchResults(
+                sort_on='effective',
+                sort_order='ascending',
+                sort_limit=1,
+                effective={
+                    'query': (
+                        DateTime('1980-01-01 00:00:00'),
+                    ),
+                    'range': 'min',
+                }
+        )[0].effective.year()
+        last = catalog.searchResults(
+                sort_on='effective',
+                sort_order='descending',
+                sort_limit=1
+        )[0].effective.year()
+
+        years = range(first, last, 1)
+        years.reverse()
+        return years
+
+    def vocabulary(self):
+        years = self.years
+        return [(str(i), str(i)) for i in years]
+
+    def count(self, brains, sequence=None):
+        """ Intersect results
+        """
+        res = {}
+        if not sequence:
+            sequence = self.years
+
+        if not sequence:
+            return res
+
+        index_id = self.data.get('index')
+        if not index_id:
+            return res
+
+        ctool = getToolByName(self.context, 'portal_catalog')
+        index = ctool._catalog.getIndex(index_id)
+        ctool = queryUtility(IFacetedCatalog)
+        if not ctool:
+            return res
+
+        brains = IISet(brain.getRID() for brain in brains)
+        res[""] = res['all'] = len(brains)
+        for value in sequence:
+            if not value:
+                res[value] = len(brains)
+                continue
+            year = int(value)
+            start = DateTime(year, 1, 1)
+            end = DateTime(year, 12, 31).latestTime()
+            query = {
+                'query': (start, end),
+                'range': 'min:max'
+            }
+            rset = ctool.apply_index(self.context, index, query)[0]
+            rset = IISet(rset)
+            rset = weightedIntersection(brains, rset)[1]
+            res[value] = len(rset)
+        return res
+
+
     def query(self, form):
         """ Get value from form and return a catalog dict query
         """
@@ -86,11 +114,17 @@ class Widget(CountableWidget):
         if self.hidden:
             value = self.default
         else:
-            value = form.get(self.data.getId(), '')
+            value = int(form.get(self.data.getId(), 0))
 
         if not value:
             return query
 
-        value = atdx_normalize(value)
-        query[index] = value
+        # The date indexes do not support selecting for example year.
+        # So we make a range from first to last day of the year.
+        start = DateTime(value, 1, 1)
+        end = DateTime(value, 12, 31).latestTime()
+        query[index] = {
+            'query': (start, end),
+            'range': 'min:max'
+        }
         return query
