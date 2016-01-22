eab55ccc7f2a4abab35952f3b986e25c1699d638
Coen van der Kamp <c.van.der.kamp@zestsoftware.nl>
2016-01-22 12:13:05 +0100

    Add theme view and default page.

---
+++ src/Products.minaraad/Products/minaraad/browser/theme.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import logging
+from Products.Five.browser import BrowserView
+from zope.cachedescriptors.property import Lazy
+from plone import api
+
+logger = logging.getLogger('Products.minaraad')
+
+
+class ThemeListView(BrowserView):
+    """
+    Homepage View.
+    """
+
+    def __call__(self):
+        return self.index()
+
+    @Lazy
+    def portal(self):
+        return api.portal.get()
+
+    def themes(self):
+        brains = api.content.find(
+            context=self.portal,
+            portal_type="Theme",
+        )
+        return [brain.getObject() for brain in brains]
+
+    def primary_themes(self):
+        return self.themes()[:6]
+
+    def secondary_themes(self):
+        return self.themes()[6:]
