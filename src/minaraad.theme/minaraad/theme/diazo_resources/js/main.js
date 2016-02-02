1eb96123ac5b26e7b372d30d4881135b8fecb149
Maurits van Rees <maurits@vanrees.org>
2016-02-02 19:23:31 +0100

    Fixed jshint warnings/errors in main.js.

--- src/minaraad.theme/minaraad/theme/diazo_resources/js/main.js
+++ src/minaraad.theme/minaraad/theme/diazo_resources/js/main.js
@@ -4,19 +4,19 @@
 // Self wrapping closure
 // Set strict on our scope.
 (function(){
-    "use strict";
+  "use strict";
 
-    // Is everybody ready?
-    $(function() {
-        $('.faceted-results').bind("DOMSubtreeModified", function() {
-            if ($('.masonry').children().length == 0)
-                return;
-            setTimeout(function(){
-                $('.masonry').masonry();
-                console.log('called masonry')
-            }, 1000);
+  // Is everybody ready?
+  $(function() {
+    $('.faceted-results').bind("DOMSubtreeModified", function() {
+      if ($('.masonry').children().length === 0) {
+        return;
+      }
+      setTimeout(function(){
+        $('.masonry').masonry();
+      }, 1000);
 
-        });
+    });
   });
 
 })(); // end scope.
